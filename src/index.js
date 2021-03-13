cf2545a7e6224fc79c0528f3e94454f077ebdeb1
Oleg Borovikov <hhyahhh@gmail.com>
2021-03-14 01:13:45 +0300

    Implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function findCloseBr(openBr, bracketsConfig) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] == openBr) {
+            return bracketsConfig[i][1];
+        }
+    }
+    return '';
+}
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        // 1.проверить закрытие скобки
+        if (stack.length > 0 && str[i] == stack[stack.length - 1]) {
+            stack.pop();
+            continue;
+        }
+        // 2.проверить открытие скобки
+        let closeBr = findCloseBr(str[i], bracketsConfig);
+        if (closeBr != '') {
+            stack.push(closeBr);
+            continue;
+        }
+        // 3.иначе ошибка
+        return false;
+    }
+    //если stack пустой то успех иначе ошибка
+    return stack.length == 0;
 }
